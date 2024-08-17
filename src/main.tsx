d86d2f87c71014ea04d8c82a7e0f2e03013b510f
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2024-08-17 14:21:05 +0200

    lint: Ignore unavoidable type warnings

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,20 @@ import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import theme from './theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement && rootElement instanceof HTMLElement) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  const root = ReactDOM.createRoot(rootElement);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Error: Element with ID "root" not found or is not an HTML element.');
+}
