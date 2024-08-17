539fb94745a92c56bd10c2e7cef44182f63a7b06
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2024-08-17 14:02:21 +0200

    test: Setup the first test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // src/App.tsx
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyle from './styles/globalStyles';
